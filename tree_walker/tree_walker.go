b44b5c9f81d7319869efacb4b9a66b48e31f9c1c
antham <hamonanth@gmail.com>
2015-04-26 15:56:23 +0200

    Add filtering date and path abilities, rewrite walk function

--- tree_walker/tree_walker.go
+++ tree_walker/tree_walker.go
@@ -4,52 +4,87 @@ import "log"
 import "time"
 import "os"
 
-
 // All logic to browse files
 type TreeWalker struct {
+	MaxChangeTime time.Duration
+	ExcludedPaths map[string]bool
+	fileNames     []string
+}
 
+// Constructor
+func NewTreeWalker(maxChangeTime time.Duration, excludedPaths map[string]bool) *TreeWalker {
+	return &TreeWalker{
+		MaxChangeTime: maxChangeTime,
+		ExcludedPaths: excludedPaths,
+	}
 }
 
 // TreeWalker entrypoint
-func (c *TreeWalker) Process(path *string) {
+func (c *TreeWalker) Process(path *string) []string {
+	c.walk(*path)
 
-	file, fileError := os.Open(*path)
-	fileStat, fileStatError := file.Stat()
-
-	if fileError != nil || fileStatError != nil || !fileStat.IsDir() {
-		log.SetPrefix("checker | ")
-		log.Fatal(log.Ldate, " | No file found ")
-
-		return
-	}
-
-	log.Print("Analyze folder " + *path)
-
-	c.walk(file, path)
+	return c.fileNames
 }
 
 // Recursive function to browse files
-func (c *TreeWalker) walk(file *os.File, path *string) *os.File {
-	fileStat, fileStatError := file.Stat()
+func (c *TreeWalker) walk(path string) {
 
-	if fileStat.IsDir() && fileStatError == nil {
+	file, error := os.Open(path)
+	defer file.Close()
 
-		files, filesError := file.Readdir(0)
-
-		if filesError == nil {
-
-			for i := 0; i < len(files); i++ {
-				builtPath := *path + "/" + files[i].Name()
-				subFile, subFileError := os.Open(builtPath)
-
-				if subFileError == nil && subFile != nil {
-					c.walk(subFile, &builtPath)
-				}
-			}
-		}
+	if error != nil {
+		log.Fatal(log.Ldate, " - No folder found : "+path)
 	}
 
-	log.Print("Find " + file.Name())
+	if fileStat, error := os.Stat(path); error != nil || !fileStat.IsDir() {
+		log.Fatal(log.Ldate, " - No folder found : "+path)
+	}
 
-	return file
+	if filesInfo, error := file.Readdir(0); error == nil {
+
+		for i := 0; i < len(filesInfo); i++ {
+
+			fileName := filesInfo[i].Name()
+			builtPath := path + "/" + fileName
+
+			if c.filterByPattern(fileName, c.ExcludedPaths) {
+				if subFileStat, error := os.Stat(builtPath); error == nil {
+					if !subFileStat.IsDir() {
+						if c.filterFileByDate(builtPath, c.MaxChangeTime) {
+
+							c.fileNames = append(c.fileNames, builtPath)
+
+							log.Print("Find " + builtPath)
+						}
+					} else {
+						c.walk(builtPath)
+					}
+
+				}
+			}
+
+		}
+	}
+}
+
+// Filter unwanted prefix
+func (c *TreeWalker) filterByPattern(fileName string, excludedPaths map[string]bool) bool {
+	if excludedPaths[fileName] {
+		return false
+	}
+
+	return true
+}
+
+// Filter according to duration
+func (c *TreeWalker) filterFileByDate(path string, maxChangeTime time.Duration) bool {
+	if maxChangeTime == 0 {
+		return true
+	}
+
+	if stat, error := os.Stat(path); error == nil {
+		return time.Since(stat.ModTime()) < maxChangeTime
+	}
+
+	return false
 }
